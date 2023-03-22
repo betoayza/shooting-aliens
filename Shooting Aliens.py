5d1565faa17fb080a80a4f5d3a5d3de8e53ed5ef
betoayza <aaayza@gmail.com>
2023-03-22 20:40:51 -0300

    Game optimized

--- Shooting Aliens.py
+++ Shooting Aliens.py
@@ -5,24 +5,22 @@ pygame.init()
 # Setting clock
 CLOCK = pygame.time.Clock()
 dt = 0
-FPS = 60  # velocidad de los frames
+FPS = 60  # frames speed
 
-# Setting Screen
+# Setting SCREEN
 W, H = 600, 480
-screen = pygame.display.set_caption("Shooting Aliens")
-screen = pygame.display.set_mode((W, H))
+SCREEN = pygame.display.set_caption("Shooting Aliens")
+SCREEN = pygame.display.set_mode((W, H))
 
-# Setting background
-image_background = pygame.image.load("img/fondo1.png").convert()
+# Setting wallpaper
+WALLPAPER = pygame.image.load("img/fondo1.png")
 x = 0
 
-# Setting Icon game
+# Setting icon game
 icon = pygame.image.load("img/icon/icon-sa.png")
 pygame.display.set_icon(icon)
 
-# hero
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
+# hero's images
 still = pygame.image.load("img/hero/movements/still.png")
 
 walk_right = [pygame.image.load("img/hero/movements/walk-right-1.png"),
@@ -53,83 +51,100 @@ die = [
     pygame.image.load("img/hero/movements/die-2.png")
 ]
 
-# estados iniciales del hero
+# hero initial states
+player_position = pygame.Vector2(0,300)
 is_jumping = False
-is_walking_right = False
-is_walking_left = False
-is_still = True
-is_shooting = False
-is_dead = False
-is_he_hit = False
-speed_x = 50
-speed_y = 200
 steps_counter = 0
-
+is_walking = False
+speed = 200 # moving 200 pixels by type
 
 def move_player():
     global x
     global steps_counter
-    global player_pos
+    global player_position  
+    global is_jumping  
 
-    # background moving
-    x_rel = x % screen.get_rect().width
-    screen.blit(image_background, (x_rel - image_background.get_rect().width, 0))  # move image
-    if x_rel < W and steps_counter:
-        screen.blit(image_background, (x_rel, 0))
-    x -= 1  # mueve un pixel por frame a la derecha
+    # BACKGROUND MOVEMENT
+    x_rel = x % SCREEN.get_rect().width
+    SCREEN.blit(WALLPAPER,
+                (x_rel - WALLPAPER.get_rect().width, 0))  # move image
+    if x_rel < W:
+        SCREEN.blit(WALLPAPER, (x_rel, 0))
+
+    x -= 1  # move 1 pixel to left
 
     # -----
-    # verificar si hero excede la pantalla
-    if (player_pos.x < 0 or player_pos.x > 600):
-        player_pos = -player_pos
-    if (player_pos.y < 0 or player_pos.y > 480):
-        player_pos.y = -player_pos.y
+    # hero screen limits
+    if player_position.x <= 0 or player_position.x >= 550:
+        player_position.x = -player_position.x
+    if player_position.y <= 200 or player_position.y >= 350:
+        player_position.y = -player_position.y
+
     # -------
-    # mostrar movimientos hero
-    # if steps_counter == 0:
-    #    screen.blit(still, player_pos)
+    # HERO MOVEMENTS by steps counter
 
-    # actualiza contador pasos
-    if steps_counter + 1 >= 6:
-        steps_counter = 0
+    # reset steps
+    if steps_counter + 1 >= 7:
+        steps_counter = 0     
 
-    # por defecto debe caminar
-    if steps_counter >= 0 and steps_counter <= 6:
-        screen.blit(walk_right[steps_counter // 1], player_pos)
+    # if jumping is finished
+    if is_jumping and steps_counter == 3:
+        is_jumping = False
+        steps_counter = 0   
 
-    if is_jumping:
-        screen.blit(jump[steps_counter // 1], player_pos)
-        steps_counter += 1 
+    if steps_counter >= 0:
+        if is_jumping and steps_counter <= 2:
+            SCREEN.blit(jump[steps_counter // 1], player_position)        
+            steps_counter += 1                        
+        if is_walking:
+            SCREEN.blit(walk_right[steps_counter // 1], player_position)            
+        if steps_counter == 0:
+            SCREEN.blit(still, player_position)
 
+
+# Game & controls loop
 is_running = True
-# Actions & controls loop
 while is_running:
-    dt = CLOCK.tick(FPS) / 1000  # clock
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
-    # hero movements
+    # game clock in seconds
+    dt = CLOCK.tick(FPS) / 1000  
+
+    # controls
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 50 * dt
+        player_position.y -= speed * dt
         steps_counter += 1
-    if keys[pygame.K_s]:
-        player_pos.y += 50 * dt
-        steps_counter += 1
-    if keys[pygame.K_a]:
-        player_pos.x -= 50 * dt
-        steps_counter += 1
-    if keys[pygame.K_d]:
-        player_pos.x += 50 * dt
-        steps_counter += 1
-    if keys[pygame.K_SPACE]:
-        player_pos.y += 100 *dt
-        is_jumping = True
-        steps_counter -= 1 
+        is_jumping = False
+        is_walking = True        
 
-    pygame.display.update()  # actualizar pantalla
-    move_player()  # actualiza movimiento hero
+    if keys[pygame.K_s]:
+        player_position.y += speed * dt    
+        steps_counter += 1   
+        is_jumping = False
+        is_walking = True
+
+    if keys[pygame.K_a]:
+        player_position.x -= speed * dt     
+        steps_counter += 1   
+        is_jumping = False
+        is_walking = True
+
+    if keys[pygame.K_d]:
+        player_position.x += speed * dt
+        steps_counter += 1
+        is_jumping = False
+        is_walking = True
+
+    if keys[pygame.K_SPACE]:
+        player_position.y -= 200 * dt  
+        steps_counter = 0
+        is_jumping = True
+        is_walking = False        
+
+    pygame.display.update()    
+    move_player() 
 
 pygame.quit()
